65b9072ad1b016ac3363fd1c10b3c97ffe5fbc02
Viktor Kapra <viktorkapra@gmail.com>
2022-12-28 22:20:47 +0200

    Add project files.

---
+++ Train_Project/Train_Project.cpp
@@ -0,0 +1,12 @@
+﻿// Train_Project.cpp : Defines the entry point for the application.
+//
+
+#include "Train_Project.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
